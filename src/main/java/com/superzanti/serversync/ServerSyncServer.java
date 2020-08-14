86dd1f911412f47c96d9924f8002b1836a90f71d
gundami <41334632+gundami@users.noreply.github.com>
2020-08-14 22:21:28 +0800

    Update ServerSyncServer.java

--- src/main/java/com/superzanti/serversync/ServerSyncServer.java
+++ src/main/java/com/superzanti/serversync/ServerSyncServer.java
@@ -1,5 +1,7 @@
 package com.superzanti.serversync;
 
+import java.io.IOException;
+
 public class ServerSyncServer {
     public static void main(String[] args) throws IOException{
         ServerSync.main(new String[]{"server"});
