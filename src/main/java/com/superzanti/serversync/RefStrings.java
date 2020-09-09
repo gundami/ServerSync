b47f035f558b44855a943ee46ff563510082b1d1
gundami <41334632+gundami@users.noreply.github.com>
2020-09-09 22:01:17 +0800

    Update RefStrings.java

--- src/main/java/com/superzanti/serversync/RefStrings.java
+++ src/main/java/com/superzanti/serversync/RefStrings.java
@@ -2,8 +2,8 @@ package com.superzanti.serversync;
 
 public class RefStrings {
     public static final String MODID = "com.superzanti.serversync";
-    public static final String NAME = "ServerSync";
-    public static final String VERSION = "3.6.0";
+    public static final String NAME = "ServerSync自动更新器【请耐心等待更新完成】";
+    public static final String VERSION = "3.6.1";
 
     public static final String ERROR_TOKEN = "<E>";
     public static final String DELETE_TOKEN = "<D>";
